7989c32ec74a5014f81f6ef65c29e56c5e7dc571
Fahad Mohammed <mohammedfahad1905@gmail.com>
2024-01-10 22:46:21 +0530

    Time: 5 ms (79.16%), Space: 44 MB (23.91%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp = x;
+        int rev = 0;
+        if(temp > 0){
+            while(x > 0){
+                int rem = x % 10;
+                rev = rev * 10 + rem;
+                x = x / 10;
+            }
+        }
+         if(rev == temp)
+            return true;
+         else
+            return false;
+    }
+}
