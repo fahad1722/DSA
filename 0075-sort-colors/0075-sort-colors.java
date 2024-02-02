8344cf510f7bd57ba97d884206818be3b5354acb
Fahad Mohammed <mohammedfahad1905@gmail.com>
2024-02-03 00:32:25 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (33.55%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,42 +1,35 @@
 class Solution {
-    public static void heapSort(int[] arr, int n) {
-        for (int i = n / 2 - 1; i >= 0; i--) {
-            heapify(arr, n, i);
+    public static void countingSort(int[] arr, int n) {
+
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++) {
+            max = Math.max(max, arr[i]);
         }
 
-        for (int i = n - 1; i > 0; i--) {
-            int temp = arr[0];
-            arr[0] = arr[i];
-            arr[i] = temp;
+        int[] cnt = new int[max + 1];
 
-            heapify(arr, i, 0);
-        }
-    }
-
-    public static void heapify(int[] arr, int n, int i) {
-        int largest = i;
-        int left = 2 * i + 1;
-        int right = 2 * i + 2;
-
-        if (left < n && arr[left] > arr[largest]) {
-            largest = left;
+        for (int i = 0; i < n; i++) {
+            cnt[arr[i]]++;
         }
 
-        if (right < n && arr[right] > arr[largest]) {
-            largest = right;
+        for (int i = 1; i <= max; i++) {
+            cnt[i] += cnt[i - 1];
+        }
+        
+        int[] result = new int[n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            result[cnt[arr[i]] - 1] = arr[i];
+            cnt[arr[i]]--;
         }
 
-        if (largest != i) {
-            int temp = arr[i];
-            arr[i] = arr[largest];
-            arr[largest] = temp;
-
-            heapify(arr, n, largest);
+        for (int i = 0; i < n; i++) {
+            arr[i] = result[i];
         }
     }
-
+    
     public void sortColors(int[] arr) {
         int n = arr.length;
-        heapSort(arr, n);
+        countingSort(arr, n);
     }
 }
