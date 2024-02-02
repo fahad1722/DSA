dd9e54a0625361d62459be0c2f90110d3a3916ad
Fahad Mohammed <mohammedfahad1905@gmail.com>
2024-02-02 23:41:04 +0530

    Time: 1 ms (24.28%), Space: 41.6 MB (60.19%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,17 +1,14 @@
 class Solution {
     public void sortColors(int[] arr) {
         int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-			int minIndex = i;
-			for (int j = i + 1; j < n; j++) {
-				if(arr[j] < arr[minIndex]) {
-					minIndex = j;
-				}
+        for (int i = 1; i < n; i++) {
+			int key = arr[i];
+			int j = i - 1;
+			while (j >= 0 && arr[j] > key) {
+				arr[j + 1] = arr[j];
+				j--;
 			}
-			// Swap the minimum element with the first element of the unsorted sub-array
-			int temp = arr[minIndex];
-			arr[minIndex] = arr[i];
-			arr[i] = temp;
+			arr[j + 1] = key;
 		}
     }
 }
