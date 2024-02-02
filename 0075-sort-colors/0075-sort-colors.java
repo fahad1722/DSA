f190e37158b53468bdaf3bf7dd39acb28dc13177
Fahad Mohammed <mohammedfahad1905@gmail.com>
2024-02-02 23:43:05 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (69.70%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,14 +1,32 @@
 class Solution {
+    public void swap(int arr[], int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public void quickSort(int arr[], int low, int high) {
+        if (low < high) {
+            int pivot = partition(arr, low, high);
+            quickSort(arr, low, pivot - 1);
+            quickSort(arr, pivot + 1, high);
+        }
+    }
+
+    public int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
     public void sortColors(int[] arr) {
         int n = arr.length;
-        for (int i = 1; i < n; i++) {
-			int key = arr[i];
-			int j = i - 1;
-			while (j >= 0 && arr[j] > key) {
-				arr[j + 1] = arr[j];
-				j--;
-			}
-			arr[j + 1] = key;
-		}
+        quickSort(arr, 0, n - 1);
     }
 }
