02242db85f67c5a9d7d65bb703b57bb268f4eada
Fahad Mohammed <mohammedfahad1905@gmail.com>
2024-02-03 00:40:24 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (60.19%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,35 +1,41 @@
 class Solution {
-    public static void countingSort(int[] arr, int n) {
+    public void countingSort(int[] arr, int exp) {
+        int n = arr.length;
+        int[] cnt = new int[10];
+        int[] output = new int[n];
 
+        for (int i = 0; i < n; i++) {
+            cnt[(arr[i] / exp) % 10]++;
+        }
+        
+        for (int i = 1; i < 10; i++) {
+            cnt[i] += cnt[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            output[cnt[(arr[i] / exp) % 10] - 1] = arr[i];
+            cnt[(arr[i] / exp) % 10]--;
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = output[i];
+        }
+    }
+     public void radixSort(int[] arr, int n) {
+         
         int max = Integer.MIN_VALUE;
+         
         for(int i = 0; i < n; i++) {
             max = Math.max(max, arr[i]);
         }
-
-        int[] cnt = new int[max + 1];
-
-        for (int i = 0; i < n; i++) {
-            cnt[arr[i]]++;
-        }
-
-        for (int i = 1; i <= max; i++) {
-            cnt[i] += cnt[i - 1];
-        }
         
-        int[] result = new int[n];
-
-        for (int i = n - 1; i >= 0; i--) {
-            result[cnt[arr[i]] - 1] = arr[i];
-            cnt[arr[i]]--;
-        }
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = result[i];
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countingSort(arr, exp);
         }
     }
-    
+
     public void sortColors(int[] arr) {
         int n = arr.length;
-        countingSort(arr, n);
+        radixSort(arr, n);
     }
 }
